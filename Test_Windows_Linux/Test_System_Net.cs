708521c6d76090e49be9ea6614bdd43a40371d06
ThomasKr17 <thomaskr2017@gmail.com>
2019-09-17 17:23:35 +0900

    1) Dotnet Core 버전을 2.1 에서 3.0으로 수정함

--- Test_Windows_Linux/Test_System_Net.cs
+++ Test_Windows_Linux/Test_System_Net.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+    2019.09.17 17:22
+    윈도우에서 실행하면 예상한 결과를 확인할 수 있었지만,
+    우분투에서 실행하면 예상하지 못한 결과(예: My address is : 127.0.1.1)가 나온다.
+ */
+using System;
 using System.Net;
 using System.Net.NetworkInformation;
 
@@ -19,7 +24,8 @@ namespace Test_Windows_Linux
                 Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
                 Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
                 Console.WriteLine("  Physical Address ........................ : {0}", adapter.GetPhysicalAddress().ToString());
-                Console.WriteLine("  Is receive only.......................... : {0}", adapter.IsReceiveOnly);
+                // ubuntu 18.04 에서는 오류가 발생한다. 원인모름.
+                //Console.WriteLine("  Is receive only.......................... : {0}", adapter.IsReceiveOnly);
                 Console.WriteLine("  Multicast................................ : {0}", adapter.SupportsMulticast);
 
                 Console.WriteLine();
