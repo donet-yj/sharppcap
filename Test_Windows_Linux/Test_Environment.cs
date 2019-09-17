8a22642c278455d4c57a9c8970d11be84d2db0b0
Thomas <thomaskr2017@gmail.com>
2019-09-17 19:42:02 +0900

    1) 추가: Environment 관련 소스 예제

---
+++ Test_Windows_Linux/Test_Environment.cs
@@ -0,0 +1,22 @@
+﻿/*
+    <Windows 실행결과>
+    -------------------------------------------------------------
+    Environment.OSVersion: Microsoft Windows NT 10.0.17763.0
+    Environment.OSVersion.Platform: Win32NT
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Windows_Linux
+{
+    class Test_Environment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Environment.OSVersion: " + Environment.OSVersion);
+            Console.WriteLine("Environment.OSVersion.Platform: " + Environment.OSVersion.Platform);
+        }      
+    }
+}
