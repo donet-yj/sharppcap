e5d318b43695d2ce658dec422010e1c8d3a57cad
Thomas <thomaskr2017@gmail.com>
2019-09-17 16:58:46 +0900

    1) 테스트 코드 추가함. (네트웍 IP 확인용 코드)

---
+++ Test_Windows_Linux/Test_System_Net.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Test_Windows_Linux
+{
+    class Test_System_Net
+    {
+        static void Main(string[] args)
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}.{1}     ",
+                    computerProperties.HostName, computerProperties.DomainName);
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+                Console.WriteLine(adapter.Description);
+                Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
+                Console.WriteLine("  Physical Address ........................ : {0}", adapter.GetPhysicalAddress().ToString());
+                Console.WriteLine("  Is receive only.......................... : {0}", adapter.IsReceiveOnly);
+                Console.WriteLine("  Multicast................................ : {0}", adapter.SupportsMulticast);
+
+                Console.WriteLine();
+            }
+
+
+
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress addr in localIPs)
+            {
+                if (addr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine($"My address is : {addr}");
+                }
+            }
+        }
+    }
+}
